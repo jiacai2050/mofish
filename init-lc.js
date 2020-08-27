3fef1690ceff15a84f60be47a4b25892de27648a
jiacai2050 <jiacai2050@gmail.com>
2020-08-27 20:07:23 +0800

    more strict ACL. better security

--- init-lc.js
+++ init-lc.js
@@ -3,4 +3,7 @@ const AV = require('leanengine');
 AV.init({
   appId: process.env.LEANCLOUD_APP_ID,
   appKey: process.env.LEANCLOUD_APP_KEY,
+  masterKey: process.env.LEANCLOUD_MASTER_KEY,
 });
+
+AV.Cloud.useMasterKey();
