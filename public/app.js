a1fd241a0b791255a24cd68f45f415eee7146400
jiacai2050 <jiacai2050@gmail.com>
2020-08-29 13:45:11 +0800

    add hackernews in index.html

--- public/app.js
+++ public/app.js
@@ -1,4 +1,56 @@
+
 (function($) {
+  function fetch_v2ex(start, end, table_id) {
+    console.log(start,end,table_id);
+    let q = new AV.Query('v2ex');
+    q.greaterThanOrEqualTo('created', start);
+    q.lessThan('created', end);
+    q.descending('replies');
+    q.find().then(function(results) {
+      let table = $(`#${table_id}`);
+      let posts = results;
+      console.log(posts);
+      if (posts.length < 1) {
+        alert(`${current_day}数据没有采集，请换一天浏览。`);
+      } else {
+        for (let p of posts) {
+          console.log(p);
+          p = p.attributes;
+          table.append(`<tr><td>${p['replies']}</td><td><a href='${p.url}'>${p['title']}</a></td></tr`);
+        }
+      }
+
+    }, function(error) {
+      alert(JSON.stringify(error));
+    });
+  }
+
+  function fetch_hackernews(start, end, table_id) {
+    console.log(start,end,table_id);
+    let q = new AV.Query('hackernews');
+    q.greaterThanOrEqualTo('time', start);
+    q.lessThan('time', end);
+    q.limit(25);
+    q.descending('score');
+    q.find().then(function(results) {
+      let table = $(`#${table_id}`);
+      let posts = results;
+      console.log(posts);
+      if (posts.length < 1) {
+        alert(`hackernews ${current_day}数据没有采集，请换一天浏览。`);
+      } else {
+        for (let p of posts) {
+          console.log(p);
+          p = p.attributes;
+          table.append(`<tr><td><a href="https://news.ycombinator.com/item?id=${p['id']}">${p['score']}</a></td><td><a href='${p.url}'>${p['title']}</a></td></tr`);
+        }
+      }
+
+    }, function(error) {
+      alert(JSON.stringify(error));
+    });
+  }
+
   var APP_ID = 'vUbVDkqX7D3l5nlGrMB2YNga-gzGzoHsz';
   var APP_KEY = 'hSeyI7bPX7rENUHyCzNDuyK8';
 
@@ -17,10 +69,7 @@
   tommorrow = moment(current_day).add(1, 'd');
   let yesterday_str =  yesterday.format('YYYYMMDD');
   let tommorrow_str =  tommorrow.format('YYYYMMDD');
-  let q = new AV.Query('v2ex');
-  q.greaterThanOrEqualTo('created', today.unix());
-  q.lessThan('created', tommorrow.unix());
-  q.descending('replies');
+
 
   const KEY_LEFT = 37;
   const KEY_UP = 38;
@@ -35,21 +84,9 @@
       let which_day = prompt("Which day to go?", yesterday_str);
       window.location = `${path}?day=${which_day}`;
     }
-    let table = $('#content');
-    q.find().then(function(results) {
-      let posts = results;
-      if (posts.length < 1) {
-        alert(`${current_day}数据没有采集，请换一天浏览。`);
-      } else {
-        for (let p of posts) {
-          p = p.attributes;
-          table.append(`<tr><td>${p['replies']}</td><td><a href='${p.url}'>${p['title']}</a></td></tr`);
-        }
-      }
 
-    }, function(error) {
-      alert(JSON.stringify(error));
-    });
+    fetch_v2ex(yesterday.unix(), today.unix(), 'v2ex_table');
+    fetch_hackernews(yesterday.unix(), today.unix(), 'hackernews_table');
 
     let path = window.location.pathname;
     $("#prev").attr('href', `${path}?day=${yesterday_str}`);
