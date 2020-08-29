97f636153d17b209fa4bd2856b4cebd2e88a5553
jiacai2050 <jiacai2050@gmail.com>
2020-08-29 12:14:06 +0800

    add hackernew to mail

--- actions/hacker-news/mail.js
+++ actions/hacker-news/mail.js
@@ -10,7 +10,7 @@ const file_opts = {'encoding': 'utf8', 'flags': 'a'};
 const git_sha = process.env.GIT_SHA || 'master';
 
 const myArgs = process.argv.slice(2);
-const output = myArgs[0] || 'v2ex_result.html';
+const output = myArgs[0] || 'hackernews_result.html';
 const file_console = new Console(fs.createWriteStream(output, file_opts));
 
 async function fetch_post() {
@@ -18,31 +18,19 @@ async function fetch_post() {
   let yesterday = moment().add(-1, 'd').startOf('day');
   // console.log(today.toString(), yesterday.toString());
   let q = new Query(POST_TABLE_NAME);
-  q.limit(1000);
-  q.greaterThanOrEqualTo('created', yesterday.unix());
-  q.lessThan('created', today.unix());
-  q.descending('replies');
+  q.limit(50);
+  q.greaterThanOrEqualTo('time', yesterday.unix());
+  q.lessThan('time', today.unix());
+  q.descending('score');
 
   let results = await q.find();
   let posts = [];
   for(let post of results) {
-    let o = {
-      id: post.get('id'),
-      node: post.get('node')['title'],
-      node_url: post.get('node')['url'],
-      node_image: post.get('node')['avatar_mini'],
-      description: post.get('content_rendered'),
-      url: post.get('url'),
-      replies: post.get('replies'),
-      created: moment(post.get('created') * 1000).format('YYYY-MM-DD hh:mm:ss'),
-      title: post.get('title'),
-      author: post.get('member')['username'],
-      author_url: post.get('member')['url'],
-      author_image: post.get('member')['avatar_mini'],
-    }
+    let o = post.toJSON();
+    o['created'] = moment(post.get('time') * 1000).format('YYYY-MM-DD hh:mm:ss'),
     posts.push(o);
   }
-  let tmpl = fs.readFileSync(`${__dirname}/../../public/mail_v2ex.ejs`, file_opts);
+  let tmpl = fs.readFileSync(`${__dirname}/../../public/mail_hackernews.ejs`, file_opts);
   let body = ejs.render(tmpl, {posts: posts, git_sha: git_sha}, {});
   file_console.log(body);
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
-var app = require('./app');
 require('./init-lc')
+var app = require('./app');
+
 
 // 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
 // LeanEngine 运行时会分配端口并赋值到该变量。
