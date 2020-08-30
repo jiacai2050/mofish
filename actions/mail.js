aeefe0450fc7133c3f465f1e77467a16105677b7
jiacai2050 <jiacai2050@gmail.com>
2020-08-30 10:47:01 +0800

    remvoe hard coded post date

--- actions/mail.js
+++ actions/mail.js
@@ -5,26 +5,37 @@ const hn = require('./hacker-news/mail');
 const v2ex = require('./v2ex/mail');
 const juice = require('juice');
 const moment = require('moment');
+const {argv} = require('yargs')
+
+const github_sha = process.env.GITHUB_SHA || 'master';
+const github_repo = process.env.GITHUB_REPOSITORY || 'jiacai2050/v2ex';
+const file_opts = {'encoding': 'utf8', 'flags': 'w'};
 
 if (require.main === module) {
-  const file_opts = {'encoding': 'utf8', 'flags': 'w'};
-  const git_sha = process.env.GIT_SHA || 'master';
+  const output = argv.output || 'result.html';
+  let day = argv.day || moment().add(-1, 'd').startOf('day');
+  if (typeof day === 'number') {
+    day = moment(day + '').startOf('day');
+  }
+  // [start_ts, end_ts)
+  const day_str = day.format('YYYY-MM-DD');
+  const start_ts = day.unix();
+  const end_ts = day.add(1, 'd').unix();
+  console.log(output,  start_ts, end_ts);
 
-  const myArgs = process.argv.slice(2);
-  const output = myArgs[0] || 'result.html';
   const file_console = new Console(fs.createWriteStream(output, file_opts));
 
   (async () => {
-    let hn_posts = await hn.fetch_post();
-    let v2ex_posts = await v2ex.fetch_post();
-    let yesterday = moment().add(-1, 'd').startOf('day');
+    let hn_posts = await hn.fetch_post(start_ts, end_ts);
+    let v2ex_posts = await v2ex.fetch_post(start_ts, end_ts);
 
     let tmpl = fs.readFileSync(`${__dirname}/../public/mail.ejs`, file_opts);
     let body = ejs.render(tmpl, {
       hn_posts: hn_posts,
       v2ex_posts: v2ex_posts,
-      git_sha: git_sha,
-      data_time: yesterday.format('YYYY-MM-DD')
+      github_sha: github_sha,
+      github_repo: github_repo,
+      data_time: day_str,
     }, {views: [`${__dirname}/../public`]});
     file_console.log(juice(body));
   })()
--- actions/v2ex/mail.js
+++ actions/v2ex/mail.js
@@ -6,14 +6,11 @@ const moment = require('moment');
 const { Console } = require('console');
 const { POST_TABLE_NAME, ONLINE_TABLE_NAME } = require('./common')
 
-async function fetch_post() {
-  let today = moment().startOf('day');
-  let yesterday = moment().add(-1, 'd').startOf('day');
-  // console.log(today.toString(), yesterday.toString());
+async function fetch_post(start_ts, end_ts) {
   let q = new Query(POST_TABLE_NAME);
   q.limit(1000);
-  q.greaterThanOrEqualTo('created', yesterday.unix());
-  q.lessThan('created', today.unix());
+  q.greaterThanOrEqualTo('created', start_ts);
+  q.lessThan('created', end_ts);
   q.descending('replies');
 
   let results = await q.find();
@@ -40,16 +37,25 @@ async function fetch_post() {
 
 if (require.main === module) {
   const file_opts = {'encoding': 'utf8', 'flags': 'a'};
-  const git_sha = process.env.GIT_SHA || 'master';
+  const github_sha = process.env.GITHUB_SHA || 'master';
+  const github_repo = process.env.GITHUB_REPOSITORY || 'jiacai2050/v2ex';
 
   const myArgs = process.argv.slice(2);
   const output = myArgs[0] || 'v2ex_result.html';
   const file_console = new Console(fs.createWriteStream(output, file_opts));
 
+  let today = moment().startOf('day');
+  const start_ts = today.unix();
+  const end_ts = today.add(-1, 'd').unix();
+
   (async () => {
-    let posts = await fetch_post();
+    let posts = await fetch_post(start_ts, end_ts);
     let tmpl = fs.readFileSync(`${__dirname}/../../public/mail_v2ex.ejs`, file_opts);
-    let body = ejs.render(tmpl, {posts: posts, git_sha: git_sha}, {});
+    let body = ejs.render(tmpl, {
+      posts: posts,
+      github_sha: github_sha,
+      github_repo: github_repo
+    }, {});
     file_console.log(body);
   })()
 }
