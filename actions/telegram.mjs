d7c82641de9a144ad8913ca9bc787266886fe3aa
jiacai2050 <dev@liujiacai.net>
2025-11-23 20:07:38 +0800

    feat: send telegraph posts in telegram channel message

--- actions/telegram.mjs
+++ actions/telegram.mjs
@@ -2,7 +2,7 @@ import fs from "fs";
 import { createTelegraphPage } from "./telegraph.js";
 
 const TOKEN = process.env.TELEGRAM_TOKEN;
-const POST_TOKEN = process.env.TELEGRAPH_ACCESS_TOKEN;
+const POST_TOKEN = process.env.TELEGRAPH_TOKEN;
 const CHAT_ID = process.env.TELEGRAM_CHAT_ID || "-1002672418956";
 const MESSAGE_FILE = process.env.TELEGRAM_MESSAGE_FILE || "telegram-bot.md";
 const POST_FILE = process.env.TELEGRAPH_MESSAGE_FILE || "telegraph.md";
@@ -10,15 +10,18 @@ const POST_TITLE = process.env.TELEGRAPH_POST_TITLE || "HN Summary";
 
 async function createTelegrahPost() {
   const content = fs.readFileSync(POST_FILE, "utf8");
-  return await createTelegraphPage(POST_TOKEN, POST_TITLE, content);
+  return await createTelegraphPage(POST_TOKEN, POST_TITLE, content, {
+    authorUrl: "https://github.com/jiacai2050/mofish/",
+  });
 }
 
-async function sendMessage() {
+async function sendMessage(post_title, post_url) {
   const api = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
-  // const message = fs.readFileSync(MESSAGE_FILE, "utf8");
+  const message = fs.readFileSync(MESSAGE_FILE, "utf8");
+  const text = `View it on the web at [${normalize(post_title)}](${post_url})\n\n${message}`;
   // console.log(message);
   const payload = JSON.stringify({
-    text: message,
+    text,
     chat_id: CHAT_ID,
     parse_mode: "MarkdownV2",
   });
@@ -45,8 +48,27 @@ function normalize(title) {
 
 const resp = await createTelegrahPost();
 console.log(resp);
+// {
+//   ok: true,
+//   result: {
+//     path: 'HN-Summary-11-23',
+//     url: 'https://telegra.ph/HN-Summary-11-23',
+//     title: 'HN Summary',
+//     description: '',
+//     views: 0,
+//     can_edit: true
+//   }
+// }
 const post_url = resp.result.url;
 const post_title = resp.result.title;
-const normalized_title = normalize(post_title);
 
-await sendMessage(`*${normalized_title}*\n${post_url}`);
+try {
+  const output = process.env.GITHUB_OUTPUT || "/tmp/github_output.txt";
+  for (const [k, v] of Object.entries(resp.result)) {
+    fs.appendFileSync(output, `${k}=${v}\n`);
+  }
+} catch (e) {
+  console.warn(`Write github output failed: ${e}`);
+}
+
+await sendMessage(post_title, post_url);
