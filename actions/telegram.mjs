af08a388c6f972ef831af2e69f988522229afdc3
jiacai2050 <dev@liujiacai.net>
2025-11-23 16:32:46 +0800

    remove dot from telegram.ejs

--- actions/telegram.mjs
+++ actions/telegram.mjs
@@ -1,26 +1,27 @@
-import fs from 'fs';
+import fs from "fs";
 
 const TOKEN = process.env.TELEGRAM_TOKEN;
-const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
-const MESSAGE_FILE = process.env.TELEGRAM_MESSAGE_FILE;
+const CHAT_ID = process.env.TELEGRAM_CHAT_ID || "-1002672418956";
+const MESSAGE_FILE = process.env.TELEGRAM_MESSAGE_FILE || "telegram-bot.md";
 const api = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
-const message = fs.readFileSync(MESSAGE_FILE, 'utf8');
+const message = fs.readFileSync(MESSAGE_FILE, "utf8");
 console.log(message);
 const payload = JSON.stringify({
-    'text': message,
-    'chat_id': CHAT_ID,
-    'parse_mode': 'MarkdownV2',
-    'link_preview_options': { 'is_disabled': true }
+  text: message,
+  chat_id: CHAT_ID,
+  parse_mode: "MarkdownV2",
+  link_preview_options: { is_disabled: true },
 });
+
 const ret = await fetch(api, {
-  method: 'POST',
+  method: "POST",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   body: payload,
 });
 
-if(!ret.ok) {
+if (!ret.ok) {
   throw new Error(await ret.text());
 }
